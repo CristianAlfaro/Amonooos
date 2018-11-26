0c23a22e298ea17b230ecd1b542c91c14fc073bf
DARUBI <dilverpro@gmail.com>
2018-11-26 17:03:40 -0600

    chat

---
+++ chat/src/public/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    //alert('works')
+    const socket =io();
+    //obteniedno elementos del DOM
+    const $messageForm = $('#message-form');
+    const $messageBox=$('#message');
+    const $chat=$('#chat');
+
+    $messageForm.submit( e => {//enviar al sevidor
+        e.preventDefault();
+        //console.log('enviando datos');
+        //console.log($messageBox.val());
+        socket.emit('send message',$messageBox.val());
+        $messageBox.val('');    
+    });
+    //recibir del servidor
+    socket.on('nuevo',function(data){
+        console.log(data);
+        $chat.append(data +'<br/>');
+    });
+
+
+})
