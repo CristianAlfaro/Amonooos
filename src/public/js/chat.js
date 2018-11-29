98f7a4428557857ce8e1e8126f66d8b0753ba1b1
DARUBI <dilverpro@gmail.com>
2018-11-29 15:08:14 -0600

    chat cambio de p a li y disenio

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -6,6 +6,7 @@ $(function(){
     const $messageForm = $('#message-form');
     const $messageBox=$('#message');
     const $chat=$('#chat');
+    const $user=$('#user');
 
     $messageForm.submit( e => {//enviar al sevidor
         e.preventDefault();
@@ -15,11 +16,17 @@ $(function(){
         $messageBox.val('');    
     });
     //recibir del servidor
-    socket.on('nuevo',function(req,res){
-        console.log(req);
-       // $chat.append(req +'<br/>');
-        $('<li class="replies"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + req + '</p></li>').appendTo($('.messages ul'));
-        //$('<li class="replies"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + req + '</p></li>').appendTo($('.messages ul'));
+    /////////////////////emisor////////////
+    socket.on('nuevo mensaje',function(req,res){
+       // console.log(req);
+        let usuario=document.getElementById('#user');       
+        let contenido =  document.querySelector("#chat");
+        console.log("usuario es "+usuario);
+     console.log(contenido);
+     contenido.innerText="";
+        $chat.append(req);
+        $('<li class="replies"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + req +'<br>' +Date()+ '</br></p></li>').appendTo($('.messages ul'));
+        //$('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + req + '</p></li>').appendTo($('.messages ul'));
 
     });
 
